bc68c0e26978b7e04f208ed187e0f4073136d155
Shahidullah <117352077+Shahidullahamin@users.noreply.github.com>
2022-11-11 05:34:56 -0800

    collection
    collection is added

---
+++ collection.java
@@ -0,0 +1,21 @@
+package javaworking1;
+
+import java.util.ArrayList;
+
+public class collection {
+
+	public static void main(String[] args) {
+		ArrayList list= new ArrayList();
+
+	list.add("shahid");
+	list.add(1000);
+	list.add('s');
+	list.add(null);
+	list.add(300);
+	
+	
+	
+	System.out.println(list.toString());
+	
+	}
+}
